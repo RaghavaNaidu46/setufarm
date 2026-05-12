e75b8bf9b9b8470da61a6a68a18228542adcbdfc
Aravindterl <aravind.terli@techoptima.ai>
2026-05-12 15:47:07 +0530

    Added webiste

--- website/src/components/Navbar.tsx
+++ website/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 const APP_URL = 'http://localhost:5173';
